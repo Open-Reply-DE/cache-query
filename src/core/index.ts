0b26c00c2ba4372705fcbefdb194c870ca230a39
Armin Jazi <a.ghajarjazi@reply.de>
2024-06-11 15:53:14 +0200

    add build and publish scripts

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,13 @@
 import GlobalSyncEvent, { GlobalSyncEventType } from "~/core/global-sync-event";
+import useQuery from "~/core/use-query";
+import { DefaultCache } from "~/core/default-cache";
+import { DefaultCacheSync } from "~/core/default-cache-sync";
 
 declare global {
   interface WindowEventMap {
     [GlobalSyncEventType]: GlobalSyncEvent<unknown>;
   }
 }
+
+export { GlobalSyncEvent, GlobalSyncEventType, useQuery, DefaultCache, DefaultCacheSync };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vitest/config";
 import { fileURLToPath, URL } from "url";
+import { resolve } from "path";
 
 export default defineConfig({
-
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),
@@ -11,4 +11,12 @@ export default defineConfig({
   test: {
     environment: "happy-dom",
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/core/index.ts"),
+      name: "CacheQuery",
+      fileName: "index",
+    },
+    outDir: "dist",
+  }
 });
